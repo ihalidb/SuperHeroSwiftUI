89a62eaf45a3c3bee7321a9d1fb98e13c5d645dd
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-16 21:47:52 +0300

    Initial Commit

---
+++ SuperHeroSwiftUI/SuperHeroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroSwiftUIApp.swift
+//  SuperHeroSwiftUI
+//
+//  Created by İbrahim Halid Bayrak on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
